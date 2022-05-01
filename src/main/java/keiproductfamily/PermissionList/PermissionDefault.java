133cb1106968de2fcc5af1fd65d01b3122d7b0fe
KumaYamamoto <kumaspeed110@gmail.com>
2022-05-01 18:19:59 +0900

    PermissionDefaultの中身を一般化

--- src/main/java/keiproductfamily/PermissionList/PermissionDefault.java
+++ src/main/java/keiproductfamily/PermissionList/PermissionDefault.java
@@ -4,114 +4,24 @@ class PermissionDefault {
     static String defaultHodler =
             "#2021/08/03\n" +
                     "PermissionFileReLoad{\n" +
-                    " ServerMaintainer\n" +
-                    " KEI\n" +
+                    " ExmpleCompany1\n" +
+                    " ExmpleCompany2\n" +
                     "}\n" +
-                    "AdvanceChunkLoaderBlock{\n" +
-                    " MTT\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "ChunkLoaderBlock{\n" +
-                    " MTT\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "\n" +
-                    "EnabledChunkLoaderList{\n" +
-                    " MTT\n" +
-                    " ServerMaintainer\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "\n" +
-                    "TAWSendItem{\n" +
-                    " Tokyu\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "TAWReceiveBlock{\n" +
-                    " Tokyu\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "TPRReceiveItem{\n" +
-                    " Tokyu\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "TPRSendBlock{\n" +
-                    " Tokyu\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "TPRSendItem{\n" +
-                    " Tokyu\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "ReceiverTrafficLight{\n" +
-                    " Tokyu\n" +
-                    " UnEx\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "ReceiverTurnout{\n" +
-                    " Tokyu\n" +
-                    " UnEx\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "SCWirelessAdvance{\n" +
-                    " Tokyu\n" +
-                    " UnEx\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "EntityTrainDetectorAdvance{\n" +
-                    " Tokyu\n" +
-                    " UnEx\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "TurnoutSelector{\n" +
-                    " Tokyu\n" +
-                    " UnEx\n" +
-                    " NNR\n" +
-                    " KEI\n" +
-                    "}\n" +
-                    "ReceiverTrafficLightType2{\n" +
-                    " Tokyu\n" +
-                    " UnEx\n" +
-                    " NNR\n" +
-                    " KEI\n" +
+                    "ExmpleBlock{\n" +
+                    " ExmpleCompany1\n" +
                     "}\n" +
                     "";
 
     static String defaultCompany =
             "#2021/08/03\n" +
-                    "ServerMaintainer{\n" +
-                    " anatawa12\n" +
-                    " kuma_yamamoto\n" +
+                    "ExmpleCompany1{\n" +
+                    " exmpleMember11\n" +
+                    " exmpleMember12\n" +
                     " debuger\n" +
                     "}\n" +
-                    "MTT{\n" +
-                    " kuma_yamamoto\n" +
-                    " kaka_256\n" +
-                    " Alice1537\n" +
-                    " debuger\n" +
-                    "}\n" +
-                    "Tokyu{\n" +
-                    " seemense\n" +
-                    " debuger\n" +
-                    "}\n" +
-                    "KEI{\n" +
-                    " kuma_yamamoto\n" +
-                    " debuger\n" +
-                    "}\n" +
-                    "UnEx{\n" +
-                    " kaka_256\n" +
-                    " debuger\n" +
-                    "}\n" +
-                    "NNR{\n" +
-                    " sagen_eins\n" +
+                    "ExmpleCompany2{\n" +
+                    " exmpleMember21\n" +
+                    " exmpleMember22\n" +
                     " debuger\n" +
                     "}\n"
             ;
